3b33185d754baae960deabfa530054a8c7c04a86
Joy Sarkar <joysarkar8171@gmail.com>
2024-03-08 15:37:12 +0530

    get teacher endpoint added

--- db/database.js
+++ db/database.js
@@ -1,18 +1,18 @@
 
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize({
-    dialect: 'postgres',
-    host: 'ep-late-dawn-28386266-pooler.ap-southeast-1.postgres.vercel-storage.com',
-    username: 'default',
-    password: 'FsjYCXPz4py2',
-    database: 'verceldb',
-    ssl: true,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // you can set it to true in production with a valid certificate
-      }
+  dialect: 'postgres',
+  host: 'ep-late-dawn-28386266-pooler.ap-southeast-1.postgres.vercel-storage.com',
+  username: 'default',
+  password: 'FsjYCXPz4py2',
+  database: 'verceldb',
+  ssl: true,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // you can set it to true in production with a valid certificate
     }
-  });
+  }
+});
 
-  module.exports = sequelize
+module.exports = sequelize
